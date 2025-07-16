d489b247594e8017df1d46185276653a3e6f3835
Alex Gravitos <warpless.gravitos@gmail.com>
2025-07-16 05:41:49 +0300

    WIP: multiple chat support

--- src/js/client.js
+++ src/js/client.js
@@ -5,31 +5,79 @@ const ollama = new Ollama({ host: 'http://127.0.0.1:11434' })
 const sendbutton = document.getElementById("send")
 const msg = document.getElementById("message")
 const modelpicker = document.getElementById("modelpicker")
+const chatview = document.getElementById("messages")
+const chatlist = document.getElementById("chatlist")
+const newbutton = document.getElementById("new-chat")
 
-var id = 0
-var chat = []
+var msgid = 0
+var current_chat = 0
+var chats = []
+chats[current_chat] = []
+
+function updateChatList() {
+	chatlist.innerHTML = ""
+	for (const i in chats) {
+		var classes = "chat_switcher"
+		if (i.toString() == current_chat.toString()) {
+			classes += ' active_chat'
+		}
+		var name
+		if (chats[i][0] != undefined) {name = chats[i][0].content} else {name = "Empty Chat"}
+		chatlist.innerHTML += '<li class="cs_container"><button class="' + classes + '" id="chat' + i + '">' + name + '</button></li>'
+		document.getElementById("chat"+i).onclick = function() {const c = i; setChat(c)}
+	}
+}
+
+function newChat() {
+	current_chat = chats.length
+	chats[current_chat] = []
+	updateChatList()
+	setChat(current_chat)
+}
+
+function setChat(id) {
+	current_chat = id
+	loadChat(id)
+}
+
+function loadChat(id) {
+	chatview.innerHTML = ""
+	for (const i of chats[id]) {
+		if (i.role == "user") {
+			chatview.innerHTML += '<li class="message msg_outgoing" id="message' + i + '">' + i.content + '</li>';
+		}
+		if (i.role == "assistant") {
+			chatview.innerHTML += '<li class="message msg_incoming" id="message' + i + '">' + i.content + '</li>';
+		}
+	}
+	msgid = chats[id].length
+}
 
 async function sendQuery() {
 	var query = msg.value;
 	console.log(query);
-	document.getElementById("messages").innerHTML += '<li class="message msg_outgoing" id="message' + id + '">' + query + '</li>';
-	id += 1;
+	chatview.innerHTML += '<li class="message msg_outgoing" id="message' + msgid + '">' + query + '</li>';
+	msgid += 1;
 	msg.value = "";
-	chat.push({role: 'user', content: query})
+	msg.disabled = true;
+	chats[current_chat].push({role: 'user', content: query})
 	const response = await ollama.chat({
 		model: modelpicker.value,
-		messages: chat,
+		messages: chats[current_chat],
 		stream: true,
 	})
-	document.getElementById("messages").innerHTML += '<li class="message msg_incoming" id="message' + id + '"></li>';
-	const resp = document.getElementById("message" + id)
-	chat.push({role: 'assistant', content: ""})
+	chatview.innerHTML += '<li class="message msg_incoming" id="message' + msgid + '"></li>';
+	const resp = document.getElementById("message" + msgid)
+	chats[current_chat].push({role: 'assistant', content: ""})
 	for await (const part of response) {
 	  console.log(part)
-	  chat[chat.length - 1].content += part.message.content
-	  resp.innerHTML = converter.makeHtml(chat[chat.length - 1].content)
+	  chats[current_chat][chats[current_chat].length - 1].content += part.message.content
+	  resp.innerHTML = converter.makeHtml(chats[current_chat][chats[current_chat].length - 1].content)
 	}
-	id += 1
+	msgid += 1
+	msg.disabled = false;
+	localStorage.setItem("chats", JSON.stringify(chats))
+	updateChatList()
 }
 
 async function updateModelList() {
@@ -46,6 +94,11 @@ msg.addEventListener('keydown', function(event) {
   }
 });
 sendbutton.onclick = function() {sendQuery()}
+newbutton.onclick = function() {newChat()}
 
 updateModelList()
 
+chats = JSON.parse(localStorage.getItem("chats"))
+console.log(chats)
+loadChat(chats.length - 1)
+updateChatList()
